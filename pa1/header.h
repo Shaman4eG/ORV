1906d37acc08f47ac70fca2f74bd156c24bc94e3
Unknown <vladbakshen@mail.ru>
2018-06-14 22:39:11 +0400

    AND ANOTHER ONE

--- pa1/header.h
+++ pa1/header.h
@@ -13,10 +13,10 @@
 #include <time.h>
 #include <unistd.h>
 
-void closeUsedPipes(int id, Process *cp, FILE *flog);
-void closeUnusedPipes(int id, int count, FILE *flog, Process *cp);
+void closeUsedPipes(int id, Process *cp, FILE *LoggingFile);
+void closeUnusedPipes(int id, int count, FILE *LoggingFile, Process *cp);
 void eventLog(FILE * stream, const char *fmt, ...);
-int child (int id, Process *pipeList, FILE *flog, FILE *ef);
+int child (int id, Process *pipeList, FILE *LoggingFile, FILE *EventsLoggingFile);
 int receiveAll(Process *cp, int count, MessageType type, int *anotherCnt);
 
 static const char * const UsedPipeReadCloseFmtLog       = "%u Closing used pipe %d read %d in process %d\n";
@@ -28,7 +28,6 @@ static const char * const UnusedPipeReadCloseFmtLog       = "%u Closing unused p
 static const char * const UnusedPipeWriteErrorCloseFmtLog = "%u Error closing unused pipe %d write %d in process %d with error: %s\n";
 static const char * const UnusedPipeWriteCloseFmtLog      = "%u Closing unused pipe %d write %d in process %d\n";
 static const char * const UsedPipeReadErrorCloseFmtLog  = "%u Error closing used pipe %d read %d in process %d with error: %s\n";
-
 
 typedef struct {
   int in;
