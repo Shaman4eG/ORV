3fb231539496d0c4b2fd9b21bad8d4a5d5312f2d
Unknown <vladbakshen@mail.ru>
2018-06-14 22:40:15 +0400

    AND ANOTHER ONE BITES THE DUST

--- pa1/communicate.c
+++ pa1/communicate.c
@@ -1,164 +1,152 @@
 #include <stdarg.h>
 #include "header.h"
 
-void eventLog(FILE * stream, const char * fmt, ...) {
-  va_list args, args2;
-  va_start(args, fmt);
-  va_copy(args2, args);
-  vfprintf(stream, fmt, args);
-  vfprintf(stdout, fmt, args2);
-  va_end(args);
+void eventLog(FILE * stream, const char * fmt, ...) 
+{
+	va_list args, args2;
+	va_start(args, fmt);
+	va_copy(args2, args);
+	vfprintf(stream, fmt, args);
+	vfprintf(stdout, fmt, args2);
+	va_end(args);
 }
 
-ssize_t continiousWrite (int fd, const void *buf, size_t len) {
-  ssize_t ret, sndd = 0;
-  unsigned char *addr = (unsigned char *)buf;
-  //ret = write(fd, buf, len);
-  while(len != 0 && (ret = write(fd, addr, len)) != 0) {
-    if(ret == -1) {
-      if(errno == EINTR)
-        continue;
-      return -1;
-    }
+int send(void * self, local_id dst, const Message * msg) 
+{
+	pipeDescriptor *descriptor = (pipeDescriptor*) self;
+	size_t len = sizeof (MessageHeader) + msg->s_header.s_payload_len * sizeof(char);
 
-    len -= ret;
-    addr += ret;
-    sndd += ret;
-  }
-  return sndd;
+	ssize_t returned, bytesSended = 0;
+	unsigned char *buf = (unsigned char *)msg;
+
+	while (len != 0 && (returned = write(descriptor->out, buf, len)) != 0)
+	{
+		if (returned == -1) 
+			return -1;
+
+		len -= returned;
+		buf += returned;
+		bytesSended += returned;
+	}
+
+	return 0;
 }
 
-int send(void * self, local_id dst, const Message * msg) {
-  pipeDescriptor *pd = ( pipeDescriptor *)self;
-  size_t len = sizeof (MessageHeader) + msg->s_header.s_payload_len * sizeof(char);
+int send_multicast(void * self, const Message * msg) 
+{
+	Process *descriptor = (Process*) self;
+	for(int i = 0; i < descriptor->count; i++) 
+	{
+		if(descriptor->pipes[i].out != -1) 
+		{
+				if(send(&descriptor->pipes[i], i, msg) == -1) 
+				{
+				perror("multicast error\n");
+				return -1;
+				}
+		}
+	}
 
-  if(continiousWrite (pd->out, msg, len) == -1) {
-      perror("send error\n");
-      return -1;
-  }
-
-  return 0;
+	return 0;
 }
 
-int send_multicast(void * self, const Message * msg) {
-  Process *pd = ( Process *)self;
-  for( int i = 0; i < pd->count; i++) {
-    if(pd->pipes[i].out != -1) {
-      if(send(&pd->pipes[i], i, msg) == -1) {
-        perror("multicast error\n");
-        return -1;
-      }
-    }
-  }
+ssize_t Readsomething (int fd, void *buf, size_t len)
+{
+	ssize_t bytesReturned;
+	size_t recieved = 0;
+	unsigned char * addr = (unsigned char *)buf;
+	
+	while (len != 0 && (bytesReturned = read(fd , addr, len)) != 0) 
+	{
+		if (bytesReturned == -1 ) 
+		{
+			return -1;
+		}
 
-  return 0;
+		len  -= bytesReturned;
+		addr += bytesReturned;
+		recieved += bytesReturned;
+	}
+	return recieved;
 }
 
-ssize_t continiousRead (int fd, void *buf, size_t len) {
-  ssize_t ret;
-  size_t rcvd = 0;
-  unsigned char * addr = (unsigned char *)buf;
-  //ret = read(fd , buff, len);
-  while (len != 0 && (ret = read(fd , addr, len)) != 0) {
-    if (ret == -1 ) {
-      if (errno == EINTR)
-        continue;
-      else if (errno == EAGAIN)
-        return -2;
-      else
-        return -1;
-    }
+int receive(void * self, local_id from, Message * msg) 
+{
+	MessageHeader msgHeader;
+	pipeDescriptor *descriptor = (pipeDescriptor*) self;
+	size_t len = sizeof(MessageHeader);
 
-    len  -= ret;
-    addr += ret;
-    rcvd += ret;
-  }
-  return rcvd;
+	ssize_t bytesReturned = Readsomething(descriptor->in, &msgHeader, len);
+	if (bytesReturned < 0)	return bytesReturned;
+
+	len = msgHeader.s_payload_len;
+
+	if(len > 0) 
+	{
+		char *payload = malloc (len * sizeof msgHeader.s_payload_len);
+
+		bytesReturned = Readsomething(descriptor->in, payload, len);
+		if ( bytesReturned < 0)	return bytesReturned;
+
+		strcpy(msg->s_payload, payload);
+		free (payload);
+	}
+	else 
+	{
+		strcpy(msg->s_payload, "");
+	}
+	msg->s_header = msgHeader;
+
+	return 0;
 }
 
-int receive(void * self, local_id from, Message * msg) {
+int receive_any(void *self, Message *msg) 
+{
+	Process *process = (Process*) self;
+	ssize_t bytesReturned = -1;
+	for(int i = 1; i < process->count; i++) 
+	{
+		if(process->pipes[i].in != -1) 
+		{
+			if((bytesReturned = receive(&process->pipes[i], i, msg)) == -1) 
+				return -1;
+			else if (bytesReturned == 0) break;
+		}
+	}
 
-  pipeDescriptor *pd = (pipeDescriptor *)self;
-  MessageHeader msgHeader;
-  size_t len = sizeof (MessageHeader);
-  ssize_t ret;
+	if (bytesReturned != 0) return -2;
 
-  ret = continiousRead(pd->in, &msgHeader, len);
-  if(ret == -1) {
-      perror ("receive error\n");
-      return ret;
-  }
-  else if (ret == -2)
-    return ret;
-
-  len = msgHeader.s_payload_len;
-
-  if(len > 0) {
-    char *payload = malloc (len * sizeof msgHeader.s_payload_len);
-
-    ret = continiousRead(pd->in, payload, len);
-    if ( ret == -1) {
-      perror("receive payload error");
-      return ret;
-    }
-    else if (ret == -2)
-      return ret;
-
-    strcpy(msg->s_payload, payload);
-    free (payload);
-  }
-  else {
-    strcpy(msg->s_payload, "");
-  }
-  msg->s_header = msgHeader;
-
-  return 0;
+	return 0;
 }
-int receive_any(void * self, Message * msg) {
 
-  Process *pd = ( Process *)self;
-  ssize_t ret = -1;
-  for(int i = 1; i < pd->count; i++) {
-    if(pd->pipes[i].in != -1) {
-      if((ret = receive(&pd->pipes[i], i, msg)) == -1) {
-        perror("any errpr\n");
-        return -1;
-      }
-      else if (ret == 0) break;
-      else if (ret == -2) {
-      }
-    }
-  }
+int receiveAll(Process *process, int count, MessageType msgType, int *anotherCnt) 
+{
+	Message msg;
+	int recieved = 0;
+	int anotherRecieve = 0;
 
-  if (ret != 0) return -2;
+	for(int i = 1; i < process->count && recieved < count; i++)
+	{
+		if (process->pipes[i].in != -1) 
+		{
+			if (receive(&process->pipes[i], i, &msg) == 0) 
+			{
+				if (msg.s_header.s_magic == MESSAGE_MAGIC)
+				{
+					if (msg.s_header.s_type == msgType)
+						recieved++;
+					else {
+						anotherRecieve++;
+						fprintf(stderr, "unhandled message type %d\n", msg.s_header.s_type);
+					}
+				}
+			}
+			else {
+				perror("parent receive_any error");
+			}
+		}
+	}
 
-  return 0;
-}
-int receiveAll(Process *cp, int count, MessageType type, int *anotherCnt) {
-
-  Message msg;
-  int rcv = 0;
-  int arcv = 0;
-
-  //while (rcv != count) {
-  for(int i = 1; i < cp->count && rcv < count; i++)
-    if(cp->pipes[i].in != -1) {
-      if (receive(&cp->pipes[i], i, &msg) == 0) {
-        if (msg.s_header.s_magic == MESSAGE_MAGIC) {
-          if(msg.s_header.s_type == type)
-              rcv++;
-          else {
-              arcv++;
-              fprintf(stderr, "unhandled message type %d\n", msg.s_header.s_type);
-            }
-          }
-      }
-      else {
-        perror("parent receive_any error");
-      }
-
-    }
-
-  *anotherCnt = arcv;
-  return 0;
+	*anotherCnt = anotherRecieve;
+	return 0;
 }
