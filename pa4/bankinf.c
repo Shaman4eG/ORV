71eea055d6dc62f1c5ddb657035c172d263f4abe
Unknown <vladbakshen@mail.ru>
2018-06-15 05:07:47 +0400

    NET, OTVECHAET POVAR

--- pa4/bankinf.c
+++ pa4/bankinf.c
@@ -4,41 +4,43 @@
 
 static timestamp_t localTime = 0;
 
-void transfer (void *parent_data, local_id src, local_id dst, balance_t amount) {
-  TransferOrder tOrder = {
-    .s_src = src,
-    .s_dst = dst,
-    .s_amount = amount
-  };
+void transfer(void *parent_data, local_id source, local_id destination, balance_t amount) 
+{
+	TransferOrder tOrder = {
+		.s_src = source,
+		.s_dst = destination,
+		.s_amount = amount
+	};
 
-  noteEventForLamportTime();
-  const Message *msg = createMessage((char *) &tOrder,
-                              sizeof (TransferOrder),
-                              TRANSFER,
-                              get_lamport_time()
-                              );
-  send(parent_data, src, msg);
-  free((char *)msg);
-  Message received;
-  receive(parent_data, dst, &received);
-  if (received.s_header.s_magic != MESSAGE_MAGIC && received.s_header.s_type != ACK){
-      fprintf(stderr, "tranfer error\n");
-  }
-  synchronizeLamportTime(received.s_header.s_local_time);
+	noteEventForLamportTime();
+	const Message *msg = createMessage((char *) &tOrder, sizeof (TransferOrder), TRANSFER, get_lamport_time());
+	send(parent_data, source, msg);
+	free((char*)msg);
 
+	Message received;
+	receive(parent_data, destination, &received);
+
+	if (received.s_header.s_magic != MESSAGE_MAGIC && received.s_header.s_type != ACK)
+	{
+		fprintf(stderr, "tranfer error\n");
+	}
+ 
+	synchronizeLamportTime(received.s_header.s_local_time);
 }
 
-
-void synchronizeLamportTime(timestamp_t time){
+void synchronizeLamportTime(timestamp_t time)
+{
     if (time > localTime)
         localTime = time;
-    localTime++;
+	noteEventForLamportTime();
 }
 
-void noteEventForLamportTime() {
-    localTime++;
+void noteEventForLamportTime() 
+{
+	localTime = localTime + 1;
 }
 
-timestamp_t get_lamport_time() {
+timestamp_t get_lamport_time() 
+{
     return localTime;
 }
